e609bf06ac4d884ba09f5e73d74acc5b74b5b546
itzel <itzelmartinez455@gmail.com>
2021-11-30 22:58:00 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Usando modelos en angular';
-
+ 
   public inventario : any=[
     {id:1, nombre:"Pantalon", precio:250},
     {id:2, nombre:"falda", precio:500}
   ];
-
-  hayRegistros():any {
-    return this.inventario.length>0;              
-  }
 
   public formulario : any = {
     id:null,
@@ -40,46 +37,48 @@ export class AppComponent {
     }
   }
   public agregar ():void{
+
     let datoNuevo={
       id: this.formulario.id,
       nombre: this.formulario.nombre,
       precio: this.formulario.precio
     };
-
-    for(let x=0;x<this.inventario.length;x++)
-      if (this.inventario[x].id == this.formulario.id){
-        //asignamos null a todas las propiedades del objeto formulario para borrar todos los 'input' del formulario.
-        this.formulario.id=null;
-        this.formulario.nombre=null;
-        this.formulario.precio=null;
-        alert('ya existe el articulo , volver a intentar');
+    for(let index=0; index<this.inventario.length; index++)
+      if (this.inventario[index].id == this.formulario.id){
+        //mandamos a llamar a funcion de limpiar los campos para agregar uevamente
+        this.limpiar(); 
+        alert('ya existe el id , volver a intentar');
         return;
-      }
+      }   
       this.inventario.push(datoNuevo);
-      //asignamos null a todas las propiedades del objeto formulario para borrar todos los 'input' del formulario.
-      this.formulario.id=null;
-      this.formulario.nombre=null;
-      this.formulario.precio=null;  
+      //mandamos a llamar a funcion de limpiar los campos del formulario
+      this.limpiar();
       alert('Agregado con exito');
       return;
   }
 
-  public editar ():void{
+  public editar (): void{
     for (let index = 0; index <this.inventario.length; index++) {
       if (this.inventario[index].id == this.formulario.id) {
           this.inventario[index].id = this.formulario.id;
           this.inventario[index].nombre=  this.formulario.nombre;
           this.inventario[index].precio= this.formulario.precio;
-          //asignamos null a todas las propiedades del objeto formulario para borrar todos los 'input' del formulario.
-          this.formulario.id=null;
-          this.formulario.nombre=null;
-          this.formulario.precio=null;  
+          //mandamos a llamar a funcion de limpiar los campos del formulario
+          this.limpiar(); 
         alert('Se actualizo con exito');
         break;
       }
-      
+      alert('no se puede modificar')      
+      break;
     }
   }
+  public limpiar():void{
+    //asignamos null a todas las propiedades del objeto formulario para borrar todos los 'input' del formulario.
+    this.formulario.id=null;
+    this.formulario.nombre=null;
+    this.formulario.precio=null;
+  }
+}
  
 
-}
+
